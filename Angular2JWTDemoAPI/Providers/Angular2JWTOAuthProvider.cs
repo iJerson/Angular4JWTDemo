80ea4b0f53d05590fa8248cb776c0c3986780173
jAy <castrojerson21@hotmail.com>
2017-07-14 16:08:02 +0800

    Update OAuth Provider

--- Angular2JWTDemoAPI/Providers/Angular2JWTOAuthProvider.cs
+++ Angular2JWTDemoAPI/Providers/Angular2JWTOAuthProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Angular2JWTDemoAPI.Infrastructure;
+using Angular2JWTDemoAPI.Models;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using System;
@@ -12,49 +13,86 @@ namespace Angular2JWTDemoAPI.Providers
 {
     public class Angular2JWTOAuthProvider : OAuthAuthorizationServerProvider
     {
-        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
-            context.Validated();
-            return Task.FromResult<object>(null);
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+
+            if (!context.TryGetFormCredentials(out clientId, out clientSecret))
+            {
+                context.Rejected();
+                context.SetError("invalid_client", "Failed to retrieve client credentials.");
+                return;
+            }
+
+            try
+            {
+                //TODO VALIDATE CLIENT
+                Client _client = new Client()
+                {
+                    Id = clientId,
+                    AllowedGrant = OAuthGrant.ResourceOwner
+                };
+
+                context.OwinContext.Set("oauth:client", _client);
+                context.Validated(clientId);
+            }
+            catch
+            {
+                context.Rejected();
+                context.SetError("server_error");
+            }
         }
 
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
-            
-            IList<string> roles = new List<string> { "admin", "superadmin" };
-            TestAPIUser user = null;
+            Client _client = context.OwinContext.Get<Client>("oauth:client");
 
-            if (context.UserName.Equals("admin") && context.Password.Equals("admin"))
+            if (_client.AllowedGrant == OAuthGrant.ResourceOwner)
             {
-                user = new TestAPIUser
+                IList<string> roles = new List<string> { "admin", "superadmin" };
+                TestAPIUser user = null;
+
+                if (context.UserName.Equals("admin") && context.Password.Equals("admin"))
                 {
-                    UserName = "admin",
-                    EmailConfirmed = true
-                };
-            }
+                    user = new TestAPIUser
+                    {
+                        UserName = "admin",
+                        EmailConfirmed = true
+                    };
+                }
 
-            if (user == null)
+                if (user == null)
+                {
+                    context.Rejected();
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+
+                if (!user.EmailConfirmed)
+                {
+                    context.Rejected();
+                    context.SetError("invalid_grant", "User did not confirm email.");
+                    return;
+                }
+
+                var identity = new ClaimsIdentity("JWT");
+
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "Manager"));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "Supervisor"));
+
+                var ticket = new AuthenticationTicket(identity, null);
+
+                context.Validated(ticket);
+            }
+            else
             {
-                context.SetError("invalid_grant", "The user name or password is incorrect.");
-                return;
+                // Client is not allowed for the 'Resource Owner Password Credentials Grant'.
+                context.Rejected();
+                context.SetError("invalid_grant", "Client is not allowed for the 'Resource Owner Password Credentials Grant'");
             }
-
-            if (!user.EmailConfirmed)
-            {
-                context.SetError("invalid_grant", "User did not confirm email.");
-                return;
-            }
-            
-            var identity = new ClaimsIdentity("JWT");
-
-            identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
-            identity.AddClaim(new Claim("sub", context.UserName));
-            identity.AddClaim(new Claim(ClaimTypes.Role, "Manager"));
-            identity.AddClaim(new Claim(ClaimTypes.Role, "Supervisor"));
-
-            var ticket = new AuthenticationTicket(identity, null);
-
-            context.Validated(ticket);
 
         }
     }
